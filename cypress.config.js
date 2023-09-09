b7fcb288697e980589d4212c27c30ff249e2109e
Umamaheswari1311 <umasa1311@gmail.com>
2023-09-09 15:48:15 +0200

    first commit

---
+++ cypress.config.js
@@ -0,0 +1,22 @@
+const { defineConfig } = require('cypress')
+const cypressSplit = require('cypress-split')
+const cucumber = require('cypress-cucumber-preprocessor').default
+
+module.exports = defineConfig({
+  e2e: {
+    // baseUrl, etc
+    fixturesFolder:'cypress/fixtures' ,
+    supportFile: false,
+    setupNodeEvents(on, config) {
+      cypressSplit(on, config)
+
+      on('file:preprocessor', cucumber())
+      // IMPORTANT: return the config object
+      return config
+    },
+    "env": {
+      "url": "https://rahulshettyacademy.com"
+    },
+    specPattern: 'cypress/e2e/cucumber/team/Features/smoke/**/*.feature',
+  },
+})
